3d0c4469c7980007de6e273779266f4fc02d54dd
shura100 <shura100@campus.technion.ac.il>
2015-10-07 22:26:58 +0300

    Runnung with -f host_file: Server 	Cant open Kinect Client 	Cant open cudart ... Maybe it cant open cudart on Server ???
    need to make each commRanc to Deliver it's IP !!!

--- Acquisition.cpp
+++ Acquisition.cpp
@@ -52,32 +52,34 @@ int main(int argc, char **argv) {
     cout<<"MPI commSize = "<< commSize << endl;
     cout<<"MPI commRank = "<< commRank << endl;
 
-	Freenect::Freenect freenect;
-	MyFreenectDevice& device = freenect.createDevice<MyFreenectDevice>(0);
+    if(commRank != 0){
 
-//	namedWindow("rgb", CV_WINDOW_AUTOSIZE);
-	namedWindow("depth", CV_WINDOW_AUTOSIZE);
+		Freenect::Freenect freenect;
+		MyFreenectDevice& device = freenect.createDevice<MyFreenectDevice>(0);
 
-//	device.startVideo();
-	device.startDepth();
+	//	namedWindow("rgb", CV_WINDOW_AUTOSIZE);
+		namedWindow("depth", CV_WINDOW_AUTOSIZE);
 
-//	//Mat depthFromKinect(Size(640, 480), CV_16UC1);
-	Mat depthConvertedToShow(Size(640, 480), CV_8UC1);
-//	Mat rgbMat(Size(640, 480), CV_8UC3, Scalar(0));
+	//	device.startVideo();
+		device.startDepth();
+
+	//	//Mat depthFromKinect(Size(640, 480), CV_16UC1);
+		Mat depthConvertedToShow(Size(640, 480), CV_8UC1);
+	//	Mat rgbMat(Size(640, 480), CV_8UC3, Scalar(0));
 
 
-	while(true){// stream depth
-		while(!device.getDepth(depthConvertedToShow)){;}
-		cv::imshow("depth", depthConvertedToShow);
-		/*
-		 * A common mistake for opencv newcomers
-		 * is to call cv::imshow() in a loop through video frames,
-		 * without following up each draw with cv::waitKey(30).
-		 * In this case, nothing appears on screen,
-		 * because highgui is never given time
-		 * to process the draw requests from cv::imshow()
-		 */
-		cvWaitKey(1);
+		while(true){// stream depth
+			while(!device.getDepth(depthConvertedToShow)){;}
+			cv::imshow("depth", depthConvertedToShow);
+			/*
+			 * A common mistake for opencv newcomers
+			 * is to call cv::imshow() in a loop through video frames,
+			 * without following up each draw with cv::waitKey(30).
+			 * In this case, nothing appears on screen,
+			 * because highgui is never given time
+			 * to process the draw requests from cv::imshow()
+			 */
+			cvWaitKey(1);
 	}
 
 //	{//stream RGB
@@ -96,6 +98,7 @@ int main(int argc, char **argv) {
 
 //	cvDestroyWindow("rgb");
 	cvDestroyWindow("depth");
+    }
 
     MPI_CHECK(MPI_Finalize());
 
